aebfd90fca85d1f1fe40f13096a283f7a011c0fa
kleidimilson <kleidimilson@gmail.com>
2020-10-05 17:13:00 -0300

    bootstrap

--- handcode/handcode/settings.py
+++ handcode/handcode/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!8a$kespg4^+4dsyj%_)kqp&)6e)^=ou_bax@70ewiibl37f=s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
