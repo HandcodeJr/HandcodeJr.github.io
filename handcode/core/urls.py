e0416f4025c2e1391593f2b2a7f7df3ed08442ae
Débora Cristina <deboraccrist@outlook.com>
2020-10-08 14:43:37 -0300

    apagando a pagina de contato e editando o erro da imagem

--- handcode/core/urls.py
+++ handcode/core/urls.py
@@ -4,7 +4,6 @@ from core.views import home,contact,about,services,team
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contact', contact, name='contact'),
     path('about', about, name='about'),
     path('services', services, name='services'),
     path('team', team,  name='team'),
