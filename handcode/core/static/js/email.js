47172bcdf363e2bac1e925ced9e1468d2ce9ba38
josevictorp81 <josevictorp81@gmail.com>
2021-03-23 19:50:49 -0300

    corrigindo links, inserindo email.js e integration.js

--- handcode/core/static/js/email.js
+++ handcode/core/static/js/email.js
@@ -1,11 +1,11 @@
 function sendMail(params){
   var temParams = {
-      from_name: document.getElementById("nome").value,
+      from_name: document.getElementById("name").value,
       to_name: 'HandCode',
-      message: document.getElementById("menssagem").value,
+      message: document.getElementById("message").value,
       reply_to: document.getElementById("email").value,
   };
-  emailjs.send('service_no0hf5e', 'template_idfhjxi', temParams)
+  emailjs.send('service_veeelzl', 'template_kiwnh1p', temParams)
   .then(function(res){
     location.reload();
     alert("Email enviado!", res.status);
---
+++ handcode/core/static/js/integration.js
@@ -0,0 +1,3 @@
+(function() {
+    emailjs.init("user_HrWE9yy6dyBeFhxAgqcfx");
+})();
